f5a032ce02f8fd0375580a15295e42bb6bb457a0
Philipe Morais <liperapltda@gmail.com>
2024-04-12 11:45:48 -0300

    Adding theme support for techs slider

--- src/components/Techs/index.tsx
+++ src/components/Techs/index.tsx
@@ -1,37 +1,66 @@
 'use client'
 
+import { useTheme } from 'next-themes'
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import { useState } from 'react'
 
+
 interface IImageRowProps {
-  images: string[]
+  images: { url: string, name: string }[]
   isPaused: boolean
   handleMouseEnter: () => void
   handleMouseLeave: () => void
 }
+
 // Componente reutilizável para renderizar uma linha de imagens
-const ImageRow = ({ images, isPaused, handleMouseEnter, handleMouseLeave }: IImageRowProps) => (
-  <div className={`flex-shrink-0 flex items-center justify-around whitespace-nowrap w-full animate-scrollX ${isPaused ? 'paused' : ''}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-    {images.map((url, index) => (
-      <div key={index} className='grid place-items-center largura'>
-        <Image src={url} width={100} height={100} alt='' className='w-full h-full rounded-sm aspect-[10/9] py-[5px] px-[20px] object-contain' />
-      </div>
-    ))}
-  </div>
+const ImageRow = ({ images, isPaused, handleMouseEnter, handleMouseLeave }: IImageRowProps) => {
+  return (
+    <div
+      className={`flex-shrink-0 flex items-center justify-around whitespace-nowrap w-full animate-scrollX ${isPaused ? 'paused' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {images.map(({ url, name }, index) => (
+        <div key={index} className='flex items-center'>
+          <Image src={url} width={100} height={100} alt={name + 'Logo'} className='w-10 h-10' />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const BundleRow = ({ images, isPaused, handleMouseEnter, handleMouseLeave }: IImageRowProps) => (
+  <>
+    <ImageRow images={images} isPaused={isPaused} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} />
+    <ImageRow images={images} isPaused={isPaused} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} />
+  </>
 )
 
 const Techs = () => {
   const [isPaused, setIsPaused] = useState(false)
+  const { theme, resolvedTheme } = useTheme()
 
-  const row1 = [
-    '/images/jee-3.svg',
-    '/images/nestjs.svg',
-    '/images/next-js.svg',
-    '/images/nodejs-icon.svg',
-    '/images/react-2.svg',
-    '/images/tailwind-css-2.svg',
-    '/images/typescript.svg',
+  const rowDark = [
+    { url: '/images/ReactDark.svg', name: 'React' },
+    { url: '/images/NextDark.svg', name: 'Next.js' },
+    { url: '/images/AstroDark.svg', name: 'Astro' },
+    { url: '/images/TypescriptDark.svg', name: 'Typescript' },
+    { url: '/images/TailwindDark.svg', name: 'Tailwind CSS' },
+    { url: '/images/NodeDark.svg', name: 'Node.js' },
+    { url: '/images/JavaDark.svg', name: 'Java' },
+    { url: '/images/PostgresDark.svg', name: 'PostgreSQL' }
+  ]
+
+  const rowLight = [
+    { url: '/images/ReactLight.svg', name: 'React' },
+    { url: '/images/NextLight.svg', name: 'Next.js' },
+    { url: '/images/AstroLight.svg', name: 'Astro' },
+    { url: '/images/TypescriptLight.svg', name: 'Typescript' },
+    { url: '/images/TailwindLight.svg', name: 'Tailwind CSS' },
+    { url: '/images/NodeLight.svg', name: 'Node.js' },
+    { url: '/images/JavaLight.svg', name: 'Java' },
+    { url: '/images/PostgresLight.svg', name: 'PostgreSQL' }
   ]
 
   const handleMouseEnter = () => {
@@ -43,13 +72,12 @@ const Techs = () => {
   }
 
   return (
-    <section className='mt-20'>
-      <div className='w-full h-fit flex items-center justify-center flex-col'>
+    <section className='mt-6 bg-white dark:bg-[#303030] rounded-3xl p-10'>
+      <div className='w-full h-fit flex items-center justify-center'>
         <div className='flex w-full overflow-hidden mask'>
-          {/* Renderizar a primeira linha de imagens */}
-          <ImageRow images={row1} isPaused={isPaused} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} />
-          {/* Renderizar a segunda linha de imagens */}
-          <ImageRow images={row1} isPaused={isPaused} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} />
+          {theme === 'dark' || resolvedTheme === 'dark' ? <BundleRow images={rowDark} isPaused={isPaused} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} /> :
+            <BundleRow images={rowLight} isPaused={isPaused} handleMouseEnter={handleMouseEnter} handleMouseLeave={handleMouseLeave} />
+          }
         </div>
       </div>
     </section>
