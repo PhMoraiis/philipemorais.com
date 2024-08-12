01fb4aeb8b5f309af0c54814395fe0eb14fbc902
Philipe Morais <liperapltda@gmail.com>
2024-08-12 09:44:49 -0300

    Remove onpholio images and add new onsub images and translates

--- src/components/Works/Onpholio.tsx
+++ src/components/Works/Onpholio.tsx
@@ -25,7 +25,7 @@ const Works = ({ className, ...props }: CardProps) => {
 
 
   const imageBG = {
-    backgroundImage: `url(${isDark ? '/images/OnPholioDark.webp' : '/images/OnPholioLight.webp'})`,
+    backgroundImage: `url(${isDark ? '/images/OnSubDark.webp' : '/images/OnSubLight.webp'})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     border: 'none',
@@ -33,15 +33,15 @@ const Works = ({ className, ...props }: CardProps) => {
   }
 
   const mobileImageBG = {
-    backgroundImage: `url(${isDark ? '/images/OnPholioMobileDark.webp' : '/images/OnPholioMobileLight.webp'})`,
+    backgroundImage: `url(${isDark ? '/images/OnSubMobileDark.webp' : '/images/OnSubMobileLight.webp'})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     borderRadius: '0.75rem',
     border: 'none'
   }
 
-  const handleGoOnpholio = () => {
-    window.open('https://github.com/PhMoraiis/onpholio', '_blank')
+  const handleGoOnSub = () => {
+    window.open('https://github.com/PhMoraiis/onsub', '_blank')
   }
 
   const [mounted, setMounted] = useState(false)
@@ -49,15 +49,15 @@ const Works = ({ className, ...props }: CardProps) => {
   if (!mounted) return null
 
   return (<>
-    <Card onClick={handleGoOnpholio} className={cn('w-full md:w-full md:h-[330px] lg:w-full lg:h-[380px] h-[550px] shadow-xl rounded-xl cursor-pointer hidden sm:flex', className)} {...props} style={imageBG}>
+    <Card onClick={handleGoOnSub} className={cn('w-full md:w-full md:h-[330px] lg:w-full lg:h-[380px] h-[550px] shadow-xl rounded-xl cursor-pointer hidden sm:flex', className)} {...props} style={imageBG}>
       <CardHeader>
-        <CardTitle className='text-black dark:text-primary'>OnPholio</CardTitle>
-        <CardDescription className='text-black dark:text-primary'>{t('OnPholioDescription')}</CardDescription>
+        <CardTitle className='text-black dark:text-primary'>OnSub</CardTitle>
+        <CardDescription className='text-black dark:text-primary'>{t('OnSubDescription')}</CardDescription>
       </CardHeader>
       <CardFooter className='gap-3'>
         <button className="inline-flex items-center rounded-full border px-3 py-1 gap-2 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80" onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter}>
           <div className={`rounded-full w-3 h-3 bg-[#4D4DFF] mr-2 ${isHover ? 'neonDev1' : 'neonDev2'} duration-300 ease-in-out`}></div>
-          {t('OnPholioStatus')}
+          {t('OnSubStatus')}
         </button>
         <Badge size="icon"><Atom size={18} />NextJS</Badge>
         <Badge size="icon"><Hexagon color='#94C745' size={18} />NodeJS</Badge>
@@ -73,16 +73,16 @@ const Works = ({ className, ...props }: CardProps) => {
       )}
       style={mobileImageBG}
       {...props}
-      onClick={handleGoOnpholio}
+      onClick={handleGoOnSub}
     >
       <CardHeader>
-        <CardTitle className='text-black dark:text-primary'>OnPholio</CardTitle>
-        <CardDescription>{t('OnPholioDescription')}</CardDescription>
+        <CardTitle className='text-black dark:text-primary'>OnSub</CardTitle>
+        <CardDescription>{t('OnSubDescription')}</CardDescription>
       </CardHeader>
       <CardFooter className='gap-3 flex-wrap'>
         <button className="inline-flex items-center rounded-full border px-3 py-1 gap-2 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80" onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter}>
           <div className={`rounded-full w-3 h-3 bg-[#4D4DFF] mr-2 ${isHover ? 'neonDev1' : 'neonDev2'} duration-300 ease-in-out`}></div>
-          {t('OnPholioStatus')}
+          {t('OnSubStatus')}
         </button>
         <Badge size="icon"><Atom size={18} />NextJS</Badge>
         <Badge size="icon"><Hexagon color='#94C745' size={18} />NodeJS</Badge>
