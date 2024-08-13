bc8e76edbab47a11521a45080da02263d300225d
Philipe Morais <liperapltda@gmail.com>
2024-08-12 23:21:48 -0300

    add dashboard initial page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,12 +7,13 @@ import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 import { useFormState } from 'react-dom'
 import loginAction from './loginAction'
+import { OctagonAlert } from 'lucide-react'
 
 const Login = () => {
   const [error, formAction] = useFormState(loginAction, undefined)
 
   return (
-    <main className="container mx-auto max-w-lg md:max-w-2xl lg:max-w-full h-[60vh] md:h-[80vh] lg:h-[80vh] flex flex-col items-center justify-center sm:px-4">
+    <main className="container mx-auto max-w-lg md:max-w-2xl lg:max-w-full h-[60vh] md:h-[80vh] lg:h-[80vh] flex flex-col items-center justify-center sm:px-4 font-Relative">
       <div className="w-full space-y-6 text-gray-600 sm:max-w-md">
         <div className="text-center">
           <div className="space-y-2">
@@ -43,13 +44,11 @@ const Login = () => {
                 type="password"
                 placeholder="************"
               />
-              <Link href="/forgotpassword" className="mt-2 ml-auto inline-block text-sm underline">
-                Esqueceu sua senha?
-              </Link>
             </div>
             <Button type="submit" className="w-full px-4 py-2">
               Entrar
             </Button>
+            { error &&  <p className='flex gap-2 text-red-500'><OctagonAlert className='text-red-500' /> {error}.</p> }
           </form>
         </div>
       </div>
