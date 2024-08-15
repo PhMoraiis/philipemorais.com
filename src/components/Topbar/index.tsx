ac50428e67c5106fe1ae78391e2e599bbc96ef88
Philipe Morais <liperapltda@gmail.com>
2024-08-15 17:13:12 -0300

    Add new component searchbar on topbar and style with sePathname

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,10 +1,18 @@
-import { Badge, CircleUser, Hexagon, Home, LayoutDashboard, Link, LogOut, Menu, Search, Settings, ToggleLeft, User } from 'lucide-react'
+import { CircleUser, Hexagon, Home, LogOut, Menu, Search, SquareTerminal, ToggleLeft } from 'lucide-react'
 import { Button } from '../ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '../ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu'
 import { Input } from '../ui/input'
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet'
 
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import { Badge } from '../ui/badge'
+import SearchBar from '../SearchBar'
+
+
 const Topbar = () => {
+  const pathname = usePathname()
+
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <Sheet>
@@ -12,10 +20,8 @@ const Topbar = () => {
           <Button
             variant="outline"
             size="icon"
-            className="shrink-0 md:hidden"
-          >
+            className="shrink-0 md:hidden">
             <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
           </Button>
         </SheetTrigger>
         <SheetContent side="left" className="flex flex-col">
@@ -29,46 +35,35 @@ const Topbar = () => {
             </Link>
             <Link
               href="/dashboard"
-              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground ${pathname === '/dashboard' ? 'text-primary' : ''}`}
             >
               <Home className="h-5 w-5" />
               Dashboard
             </Link>
             <Link
               href="/dashboard/projects"
-              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+              className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground ${pathname === '/dashboard/projects' ? 'text-primary' : ''}`}
             >
-              <LayoutDashboard className="h-5 w-5" />
+              <SquareTerminal className="h-5 w-5" />
               Projetos
-              <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+              <Badge variant={pathname === '/dashboard/projects' ? 'default' : 'outline'} className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                 6
               </Badge>
             </Link>
             <Link
               href="/dashboard/techs"
-              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground ${pathname === '/dashboard/techs' ? 'text-primary' : ''}`}
             >
               <Hexagon className="h-5 w-5" />
               Techs
-              <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+              <Badge variant={pathname === '/dashboard/techs' ? 'default' : 'outline'} className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
                 6
               </Badge>
             </Link>
           </nav>
         </SheetContent>
       </Sheet>
-      <div className="w-full flex-1">
-        <form>
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Buscar projetos..."
-              className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-            />
-          </div>
-        </form>
-      </div>
+      <SearchBar />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button size="icon" className="rounded-full">
@@ -77,10 +72,6 @@ const Topbar = () => {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuLabel className='flex justify-between'>Minha Conta <User className='ml-2 w-4 h-4' /></DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className='flex justify-between'>Configurações <Settings className='ml-2 w-4 h-4' /> </DropdownMenuItem>
-          <DropdownMenuSeparator />
           <DropdownMenuItem className='hover:bg-red-500 hover:text-secondary flex justify-between'>Sair <LogOut className='ml-2 w-4 h-4' /> </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
