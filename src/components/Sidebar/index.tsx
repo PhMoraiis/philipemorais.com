dcd0529a80dabe3a2f60b29b245d66bc835cac60
Philipe Morais <liperapltda@gmail.com>
2024-08-15 19:01:16 -0300

    debug requests and making the counting of projects and technologies dynamic

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,40 @@
 import { Hexagon, Home, SquareTerminal, ToggleLeft } from 'lucide-react'
 import { Badge } from '../ui/badge'
 
+import { apiClient } from '@/lib/apiClient'
+import useProjectStore from '@/stores/projectStore'
+import useTechStore from '@/stores/techStore'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
 
 const Sidebar = () => {
   const pathname = usePathname()
+  const { projects, setProjects, setError: setProjectError } = useProjectStore()
+  const { techs, setTechs, setError: setTechError } = useTechStore()
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const data = await apiClient('/api/projects')
+        setProjects(data)
+      } catch (error) {
+        setProjectError('Failed to fetch projects')
+      }
+    }
+
+    const fetchTechs = async () => {
+      try {
+        const data = await apiClient('/api/techs')
+        setTechs(data)
+      } catch (error) {
+        setTechError('Failed to fetch techs')
+      }
+    }
+
+    fetchProjects()
+    fetchTechs()
+  }, [setProjects, setTechs, setProjectError, setTechError])
 
   return (
     <div className="hidden border-r bg-muted/40 md:block">
@@ -32,7 +61,7 @@ const Sidebar = () => {
               <SquareTerminal className="h-4 w-4" />
               Projetos
               <Badge variant={pathname === '/dashboard/projects' ? 'default' : 'outline'} className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                6
+                {projects.length}
               </Badge>
             </Link>
             <Link
@@ -42,7 +71,7 @@ const Sidebar = () => {
               <Hexagon className="h-4 w-4" />
               Techs
               <Badge variant={pathname === '/dashboard/techs' ? 'default' : 'outline'} className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                6
+                {techs.length}
               </Badge>
             </Link>
           </nav>
