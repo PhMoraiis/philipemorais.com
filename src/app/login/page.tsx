8ce31b375a59281c3e32ef477c6a0125e39182a6
Philipe Morais <liperapltda@gmail.com>
2025-01-23 00:16:06 -0300

    feat(login): refactor and update style in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,47 +1,66 @@
 'use client'
 
+import AnimatedGradient from '@/components/fancy/animated-gradient-with-svg'
+import { FaviconOnpholio, OnPholioLogo } from '@/components/Logos'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { ArrowLeft } from 'lucide-react'
 
 const Login = () => {
 	return (
-		<main className='container mx-auto max-w-lg md:max-w-2xl lg:max-w-full h-[60vh] md:h-[80vh] lg:h-[80vh] flex flex-col items-center justify-center sm:px-4 font-Relative'>
-			<div className='w-full space-y-6 text-gray-600 sm:max-w-md'>
-				<div className='text-center'>
-					<div className='space-y-2'>
-						<h3 className='text-gray-800 text-2xl font-bold sm:text-3xl'>
-							Faça login no OnPholio
-						</h3>
-					</div>
+		<main className='flex items-center justify-center overflow-hidden'>
+			<div className='relative h-[98vh] bg-slate-300 w-1/2 flex items-start justify-start p-10 ml-2 my-2 rounded-xl'>
+				<AnimatedGradient
+					colors={['#EC4899', '#F472B6', '#3B82F6']}
+					speed={0.05}
+					blur='medium'
+				/>
+				<div className='text-center relative flex items-start justify-center gap-2 flex-col'>
+					<OnPholioLogo width={200} />
+					<p className='text-gray-500 sm:text-lg font-RelativeBk text-left'>
+						Gerenciando o seu portfólio online
+					</p>
 				</div>
-				<div className='bg-white shadow p-4 py-6 sm:p-6 sm:rounded-lg'>
+				<div>
+					<ArrowLeft size={24} />
+					<Button>Back</Button>
+				</div>
+			</div>
+			<section className='relative mx-auto w-full text-gray-600 sm:max-w-md'>
+				<div className='bg-transparent p-4 py-6 sm:p-6 sm:rounded-lg flex flex-col justify-center'>
+					<div className='flex items-center justify-center mb-6'>
+						<FaviconOnpholio width={75} />
+					</div>
+					<div className='flex items-center justify-center mb-6'>
+						<h2 className='text-3xl text-center font-RelativeBk'>
+							Bem vindo de volta!
+						</h2>
+					</div>
 					<form className='space-y-5'>
-						<div>
-							<Label htmlFor='email'>Email</Label>
+						<div
+							className='space-y-2' // NOTE: This inline style is to show how to set the --ring variable in your CSS file in order to change the focus ring color.
+							style={{ '--ring': '234 89% 74%' } as React.CSSProperties}
+						>
 							<Input
 								id='email'
 								name='email'
 								type='email'
-								placeholder='example@onpholio.com'
+								placeholder='Seu e-mail'
 								autoComplete='off'
 							/>
 						</div>
-						<div>
-							<Label htmlFor='password'>Senha</Label>
-							<Input
-								id='password'
-								name='password'
-								type='password'
-								placeholder='************'
-							/>
+						<div
+							className='space-y-2' // NOTE: This inline style is to show how to set the --ring variable in your CSS file in order to change the focus ring color.
+							style={{ '--ring': '234 89% 74%' } as React.CSSProperties}
+						>
+							<Input id='password' placeholder='********' type='password' />
 						</div>
 						<Button type='submit' className='w-full px-4 py-2'>
 							Entrar
 						</Button>
 					</form>
 				</div>
-			</div>
+			</section>
 		</main>
 	)
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,118 +10,161 @@ const config = {
   ],
   prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
-    fontFamily: {
-      Relative: ['Relative'],
-      RelativeBd: ['Relative Bold'],
-      RelativeBk: ['Relative Book'],
-    },
-    extend: {
-      colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-        'scrollX': {
-          from: { left: 'translateX(0)' },
-          to: { transform: 'translateX(-100 %)' }
-        },
-        'shine': {
-          'from': {
-            'backgroundPosition': '0 0'
-          },
-          'to': {
-            'backgroundPosition': '-200% 0'
-          }
-        },
-        'text-shake': {
-          '15%': {
-            'transform': 'translateX(5px)'
-          },
-          '30%': {
-            'transform': 'translateX(-5px)'
-          },
-          '50%': {
-            'transform': 'translateX(3px)'
-          },
-          '80%': {
-            'transform': 'translateX(2px)'
-          },
-          '100%': {
-            'transform': 'translateX(0)'
-          }
-        },
-        'flip': {
-          'to': {
-            'transform': 'rotate(360deg)'
-          }
-        },
-        'rotate': {
-          'to': {
-            'transform': 'rotate(90deg)'
-          }
-        }
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        'scrollX': 'scrollX 40s linear infinite',
-        'shine': 'shine 4s linear infinite',
-        'text-shake': 'text-shake 1s ease 1',
-        'flip': 'flip 6s infinite steps(2, end)',
-        'rotate': 'rotate 3s linear infinite both'
-      },
-    },
+  	container: {
+  		center: true,
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
+  	fontFamily: {
+  		Relative: [
+  			'Relative'
+  		],
+  		RelativeBd: [
+  			'Relative Bold'
+  		],
+  		RelativeBk: [
+  			'Relative Book'
+  		],
+			Atelier: [
+				'Atelier'
+			],
+			Formatek: [
+				'Formatek'
+			],
+  	},
+  	extend: {
+  		colors: {
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			},
+  			scrollX: {
+  				from: {
+  					left: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(-100 %)'
+  				}
+  			},
+  			shine: {
+  				from: {
+  					backgroundPosition: '0 0'
+  				},
+  				to: {
+  					backgroundPosition: '-200% 0'
+  				}
+  			},
+  			'text-shake': {
+  				'15%': {
+  					transform: 'translateX(5px)'
+  				},
+  				'30%': {
+  					transform: 'translateX(-5px)'
+  				},
+  				'50%': {
+  					transform: 'translateX(3px)'
+  				},
+  				'80%': {
+  					transform: 'translateX(2px)'
+  				},
+  				'100%': {
+  					transform: 'translateX(0)'
+  				}
+  			},
+  			flip: {
+  				to: {
+  					transform: 'rotate(360deg)'
+  				}
+  			},
+  			rotate: {
+  				to: {
+  					transform: 'rotate(90deg)'
+  				}
+  			},
+  			'background-gradient': {
+  				'0%, 100%': {
+  					transform: 'translate(0, 0)',
+  					animationDelay: 'var(--background-gradient-delay, 0s)'
+  				},
+  				'20%': {
+  					transform: 'translate(calc(100% * var(--tx-1, 1)), calc(100% * var(--ty-1, 1)))'
+  				},
+  				'40%': {
+  					transform: 'translate(calc(100% * var(--tx-2, -1)), calc(100% * var(--ty-2, 1)))'
+  				},
+  				'60%': {
+  					transform: 'translate(calc(100% * var(--tx-3, 1)), calc(100% * var(--ty-3, -1)))'
+  				},
+  				'80%': {
+  					transform: 'translate(calc(100% * var(--tx-4, -1)), calc(100% * var(--ty-4, -1)))'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			scrollX: 'scrollX 40s linear infinite',
+  			shine: 'shine 4s linear infinite',
+  			'text-shake': 'text-shake 1s ease 1',
+  			flip: 'flip 6s infinite steps(2, end)',
+  			rotate: 'rotate 3s linear infinite both',
+  			'background-gradient': 'background-gradient var(--background-gradient-speed, 15s) cubic-bezier(0.445, 0.05, 0.55, 0.95) infinite'
+  		}
+  	}
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config
