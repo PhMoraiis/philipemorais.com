52f78b45cad8a69bf9234d8771ff31f830551597
Philipe <liperapltda@gmail.com>
2026-04-14 20:10:43 -0300

    chore: create new project

--- components/ui/bentogrid.tsx
+++ components/ui/bentogrid.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { clsx } from 'clsx'
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 
 export function BentoCard({
 	dark = false,
--- components/ui/hero-highlight.tsx
+++ components/ui/hero-highlight.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { cn } from '@/lib/utils'
-import { useMotionValue, motion, useMotionTemplate } from 'framer-motion'
+import { useMotionValue, motion, useMotionTemplate } from 'motion/react'
 import type React from 'react'
 
 export const HeroHighlight = ({
